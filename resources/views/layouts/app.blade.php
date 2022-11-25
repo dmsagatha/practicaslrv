66e0bf89d9178e294940a4c5097865d483f6a90f
Doris Morales <dmorales55@gmail.com>
2022-11-25 00:47:55 -0500

    Usuarios: Filtrar nombres-apellidos - DataTables

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.print.min.js"></script>
 
     <!-- Export Scripts -->
     <script>
@@ -89,7 +88,8 @@
           dom: 'Bfrtip',
           buttons: [
             'copy', 'csv', 'excel', 'pdf', 'print'
-          ]
+          ],
+          
           /* dom: '<"html5buttons"B>lTfgitp',
           buttons: [
             {extend: 'copy'},
