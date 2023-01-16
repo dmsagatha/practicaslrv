e2473cdb1fad91878637b6878aa3a5390f80a576
Doris Morales <dmorales55@gmail.com>
2023-01-15 20:08:22 -0500

    Importar Datos Relacionados con validaciones

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -7,39 +7,77 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rule;
 use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\Importable;
-use Maatwebsite\Excel\Concerns\SkipsFailures;
-use Maatwebsite\Excel\Concerns\SkipsErrors;
-use Maatwebsite\Excel\Concerns\SkipsOnError;
-use Maatwebsite\Excel\Concerns\SkipsOnFailure;
-use Maatwebsite\Excel\Concerns\ToCollection;
-use Maatwebsite\Excel\Concerns\WithBatchInserts;
-use Maatwebsite\Excel\Concerns\WithChunkReading;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Concerns\{Importable, SkipsFailures, SkipsErrors, SkipsOnError, SkipsOnFailure, ToCollection};
+use Maatwebsite\Excel\Concerns\{WithBatchInserts, WithChunkReading, WithHeadingRow, WithValidation};
 
-class UsersImport implements 
-  ToModel, 
+class UsersImport implements ToModel, 
   WithHeadingRow, 
   WithBatchInserts, 
   WithChunkReading, 
   WithValidation,
   SkipsOnError,
   SkipsOnFailure
-// class UsersImport implements ToCollection, WithHeadingRow
+/* class UsersImport implements ToCollection, 
+    WithHeadingRow, 
+    WithBatchInserts, 
+    WithChunkReading, 
+    WithValidation,
+    SkipsOnError,
+    SkipsOnFailure */
 {
   use Importable, SkipsErrors, SkipsFailures;
 
   // ToModel
   public function model(array $row)
   {
-    return new User([
+    /* return new User([
       'first_name' => $row['first_name'],
       'last_name'  => $row['last_name'],
       'email'      => $row['email'],
       'password'   => Hash::make($row['password'])
-    ]);
+    ]); */
+    $user = User::updateOrCreate(
+      ['email' => $row['email']],
+      [
+        'email'      => $row['email'],
+        'first_name' => $row['first_name'],
+        'last_name'  => $row['last_name'],
+        'password'   => Hash::make($row['password'])
+      ]
+    );
   }
+
+  // ToCollection
+  /* public function collection(Collection $rows)
+  {
+    foreach ($rows as $row)
+    {
+      User::updateOrCreate(
+        ['email' => $row['email']],
+        [
+          'email'      => $row['email'],
+          'first_name' => $row['first_name'],
+          'last_name'  => $row['last_name'],
+          'password'   => Hash::make($row['password'])
+        ]
+      );
+    }
+  } */
+  /* public function collection(Collection $collection)
+  {
+    $collection->each(function($row) {
+      User::query()
+          ->updateOrCreate(
+            ['email' => $row['email']],
+            [
+              'email'      => $row['email'],
+              'first_name' => $row['first_name'],
+              'last_name'  => $row['last_name'],
+              'password'   => Hash::make($row['password'])
+            ]
+          );
+    });
+  } */
 
   public function rules(): array
   {
@@ -48,7 +86,7 @@ class UsersImport implements
       '*.email'  => [
         'required', 'email', 
         Rule::unique(User::class, 'email')
-        ]
+      ]
     ];
   }
     
@@ -61,18 +99,4 @@ class UsersImport implements
   {
     return 1000;
   }
-
-  // ToCollection
-  /* public function collection(Collection $rows)
-  {
-    foreach ($rows as $row)
-    {
-      User::create([
-        'email'      => $row['email'],
-        'first_name' => $row['first_name'],
-        'last_name'  => $row['last_name'],
-        'password'   => Hash::make($row['password'])
-      ]);
-    }
-  } */
 }
